ee8e6301cd9ed7908445a5185a8c7429fca0c239
StockinTea <devinjj0@gmail.com>
2017-09-29 16:17:47 +0800

    fmt fix

--- WhoIsGod.go
+++ WhoIsGod.go
@@ -6,11 +6,10 @@ import (
 )
 
 func main() {
-	j0 := "god"
-	if isGod(j0)
-  {
-		fmt.Println("J0 is god?")
-	}
+j0 := "god"
+if isGod(j0) {
+fmt.Println("J0 is god?")
+}
 }
 
 func isGod(name string) bool { return strings.EqualFold(name, "god") }
